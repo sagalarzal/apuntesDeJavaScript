84d69c64a801d8e04a378502b3190f0599eb6d40
sagalarzal <sergiogalarzalopez@gmail.com>
2021-04-10 14:58:59 -0500

    segundo commit de los apuntes de js porque ajá

---
+++ script.js
@@ -0,0 +1,18 @@
+//feclarativa
+function miFuncion () {
+    return 3;
+}
+var a =1;
+var b =3;
+//expresiva o anonima
+var miFuncionExpresiva = function() {
+    return a + b;
+}
+function saludarEstudiante(estudiante) {
+    console.log(`Hola ${estudiante}`);
+}
+
+function sumar(a,b) {
+    var resultado = a + b;
+    return resultado;
+}
